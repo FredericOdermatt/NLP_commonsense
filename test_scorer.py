5844fe81015fc3af90d91b88ffb6bd2080c56869
Johannes Bernstorff <jbernstorff@lo-login-02.leonhard.ethz.ch>
2020-11-30 13:59:40 +0100

    nothing

--- test_scorer.py
+++ test_scorer.py
@@ -9,24 +9,21 @@ reference_path = execution_dir + "/Data/kalm_data/references/subtaskC_gold_answe
 prediction_path = execution_dir + "/Data/kalm_data/predictions/subtaskC_answers.csv"
 
 ## -------- Mover Score --------
-'''
+
 MS = MoverScore(prediction_path, reference_path)
 print("Mover Scores for similarity: ", np.mean(MS.scores))
 MS.print_bad_results()
-'''
-#vis = Visualizor()
-#vis.plot_hist(MS.scores)
 
 ## -------- BLEU Score --------
 BS = BLEUScore(prediction_path, reference_path)
 BS.print_bad_results()
 print("BLEU Scores for similarity: ", np.mean(BS.scores))
-'''
+
 
 ## -------- Rouge Score --------
 RS = RougeScore(prediction_path, reference_path)
-#print(RS._scores)
 print("Rouge Scores for similarity: ", np.mean(RS.scores))
+
 ## Plotting results
 
 vis = Visualizor()
