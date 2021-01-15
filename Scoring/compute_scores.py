8083a371d300f8a416dee1ef6656edc2468271e6
Luca Disse <ldisse@student.ethz.ch>
2021-01-15 22:12:04 +0100

    runnable on colab

--- Scoring/compute_scores.py
+++ Scoring/compute_scores.py
@@ -56,15 +56,15 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-execution_dir = os.path.dirname(os.path.abspath(__file__))
-reference_path = execution_dir + args.ref_path
-prediction_path = execution_dir + args.pred_path
+#execution_dir = os.path.dirname(os.path.abspath(__file__))
+reference_path = args.ref_path
+prediction_path = args.pred_path
 
 # In case you only want to consider the 100 sentences that were evaluated from humans this should be set to true. 
 # Currently, we only have the human evaluated files for KALM. Hence this can only be used for testing KALMs output so far.
 human_evaluated_only = False
 csv_dic = {}
-out_path = '/cluster/home/ldisse/NLP_project/NLP_commonsense/data100/machine_eval_results.csv'
+out_path = './../data100/machine_eval_results.csv'
 
 ## -------- BLEU Score --------
 if eval(args.call_BLEU):
